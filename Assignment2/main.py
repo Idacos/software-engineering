3d4ef2010d1e87f01427822116ac92ec5cbf230e
Ilton da Costa Neto <idacosneto@gmail.com>
2024-09-24 17:58:10 -0400

    feat: completed all functions for cashier and sandwich_maker, while importing them into main and completing main.

--- Assignment2/main.py
+++ Assignment2/main.py
@@ -6,14 +6,34 @@ from cashier import Cashier
 # Make an instance of other classes here
 resources = data.resources
 recipes = data.recipes
-sandwich_maker_instance = #####
-cashier_instance = ######
+sandwich_maker_instance = SandwichMaker(resources)
+cashier_instance = Cashier()
 
 
 
 
 def main():
     ###  write the rest of the codes ###
+    while True:
+        choice = input("What would you like? (small/ medium/ large/ off/ report): ").lower()
+
+        ### print(f"User choice: {choice}") ###
+
+        if choice == "off":
+            print("Turning off the machine.")
+            break
+        elif choice == "report":
+            print(f"Bread: {sandwich_maker_instance.machine_resources['bread']} slice(s)")
+            print(f"Ham: {sandwich_maker_instance.machine_resources['ham']} slice(s)")
+            print(f"Cheese: {sandwich_maker_instance.machine_resources['cheese']} ounce(s)")
+        elif choice in ["small", "medium", "large"]:
+            sandwich = recipes[choice]
+            if sandwich_maker_instance.check_resources(sandwich["ingredients"]):
+                coins_inserted = cashier_instance.process_coins()
+                if cashier_instance.transaction_result(coins_inserted, sandwich["cost"]):
+                    sandwich_maker_instance.make_sandwich(choice, sandwich["ingredients"])
+        else:
+            print("Invalid choice, please try again.")
 
 if __name__=="__main__":
     main()
--- Assignment2/sandwich_maker.py
+++ Assignment2/sandwich_maker.py
@@ -5,7 +5,15 @@ class SandwichMaker:
 
     def check_resources(self, ingredients):
         """Returns True when order can be made, False if ingredients are insufficient."""
-        #####
+        for item in ingredients:
+            quantity = ingredients[item]
+            if self.machine_resources[item] < quantity:
+                print(f"Sorry, there is not enough {item}.")
+                return False
+        return True
 
     def make_sandwich(self, sandwich_size, order_ingredients):
-        ########
+        for item in order_ingredients:
+            amount = order_ingredients[item]
+            self.machine_resources[item] -= amount
+        print(f"{sandwich_size} sandwich is ready. Bon appetit!")
